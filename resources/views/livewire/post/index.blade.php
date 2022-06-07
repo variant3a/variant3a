418feb7ee7f4a22742d2809613ba7f86244ee415
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-06-07 23:02:38 +0900

    一覧 カード下margin設定

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -22,47 +22,51 @@
         </div>
     </div>
     <div class="col-md-9">
-        <div class="card text-bg-800">
-            @if ($posts->count())
-                @foreach ($posts as $post)
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12">
-                                <h5 class="card-title">
-                                    {{ $post->title }}
-                                </h5>
+        <div class="row mb-4">
+            <div class="col-12">
+                <div class="card text-bg-800">
+                    @if ($posts->count())
+                        @foreach ($posts as $post)
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h5 class="card-title">
+                                            {{ $post->title }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="row">
+
+                                </div>
+                                <div class="row">
+
+                                </div>
+                                <div class="row">
+
+                                </div>
+                                <p class="card-text">
+                                    {!! $post->content !!}
+                                </p>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    {{ $post->created_at }}
+                                </h6>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    {{ $post->updated_at }}
+                                </h6>
                             </div>
+                            @if (!$loop->last)
+                                <hr>
+                            @endif
+                        @endforeach
+                    @else
+                        <div class="card-body">
+                            <p class="card-text">
+                                There is no articles.
+                            </p>
                         </div>
-                        <div class="row">
-
-                        </div>
-                        <div class="row">
-
-                        </div>
-                        <div class="row">
-
-                        </div>
-                        <p class="card-text">
-                            {!! $post->content !!}
-                        </p>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            {{ $post->created_at }}
-                        </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            {{ $post->updated_at }}
-                        </h6>
-                    </div>
-                    @if (!$loop->last)
-                        <hr>
                     @endif
-                @endforeach
-            @else
-                <div class="card-body">
-                    <p class="card-text">
-                        There is no articles.
-                    </p>
                 </div>
-            @endif
+            </div>
         </div>
     </div>
 </div>
