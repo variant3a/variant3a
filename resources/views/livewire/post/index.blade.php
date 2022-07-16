4e2afd1468468043266fa6adcd1d6d60b983c982
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-07-16 23:44:20 +0900

    detailsボタンがwrapする挙動を修正

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -64,11 +64,11 @@
                             <div id="{{ "post-$post->id" }}" class="card-body">
                                 <div class="row">
                                     <div class="col-12 d-flex justify-content-between">
-                                        <h5 class="ps-2 py-2 border-start border-4 border-main-500">
+                                        <h5 class="ps-2 py-2 border-start border-4 border-main-500 text-break">
                                             {{ $post->title }}
                                         </h5>
                                         <span>
-                                            <a href="{{ route('post.detail', ['id' => $post->id]) }}" class="btn text-bg-hover-main-500 border-0">
+                                            <a href="{{ route('post.detail', ['id' => $post->id]) }}" class="btn text-bg-hover-main-500 border-0 text-nowrap">
                                                 Details
                                                 <i class="bi bi-chevron-right"></i>
                                             </a>
