18251a01de426c43541eb56defe0948570d234b2
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-07-18 21:52:31 +0900

    markdown 拡張機能追加

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -3,21 +3,45 @@
 namespace App\Services;
 
 use Illuminate\Support\HtmlString;
-use League\CommonMark\CommonMarkConverter;
 use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\Autolink\AutolinkExtension;
 use League\CommonMark\Extension\Table\TableExtension;
 use League\CommonMark\Extension\DisallowedRawHtml\DisallowedRawHtmlExtension;
+use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
+use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
+use League\CommonMark\Extension\TaskList\TaskListExtension;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
 
 class MarkdownService
 {
     public static function parse($text)
     {
-        $environment = Environment::createGFMEnvironment();
+        $environment = Environment::createCommonMarkEnvironment();
 
-        $environment->addExtension(new TableExtension);
         $environment->addExtension(new DisallowedRawHtmlExtension());
+        $environment->addExtension(new AutolinkExtension());
+        $environment->addExtension(new HeadingPermalinkExtension());
+        $environment->addExtension(new TableOfContentsExtension());
+        $environment->addExtension(new TableExtension());
+        $environment->addExtension(new TaskListExtension());
 
-        $converter = new CommonMarkConverter([
+        $converter = new GithubFlavoredMarkdownConverter([
+            'renderer' => [
+                'block_separator' => "\n",
+                'inner_separator' => "\n",
+                'soft_break'      => "<br>",
+            ],
+            'commonmark' => [
+                'enable_em' => true,
+                'enable_strong' => true,
+                'use_asterisk' => true,
+                'use_underscore' => true,
+                'unordered_list_markers' => ['-', '*', '+'],
+            ],
+            'max_nesting_level' => PHP_INT_MAX,
+            'slug_normalizer' => [
+                'max_length' => 255,
+            ],
             'html_input' => 'escape',
             'allow_unsafe_links' => false,
         ], $environment);
