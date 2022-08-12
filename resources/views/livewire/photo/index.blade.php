9eaa1992fefc0fa9c993ce067944857aa971a251
yuma nishimura <sulfur.monoxide168@mgmail.com>
2022-08-12 14:29:19 +0900

    画像アップロード スタイル変更

--- resources/views/livewire/photo/index.blade.php
+++ resources/views/livewire/photo/index.blade.php
@@ -1,35 +1,40 @@
-<div class="row"
-    x-data="{ droping: false, progress: 0 }">
+<div class="row">
+    <div class="col-12">
+        <div class="row mb-3"
+            x-data="{ droping: false, progress: 0 }"
+            x-cloak>
 
-    <div class="upload-container" x-cloak>
-        <div class="position-fixed top-0 start-0 vw-100 vh-100"
-            x-on:drop.prevent="$wire.uploadMultiple(
+            <div class="col-md-10 offset-md-1 col-12 d-grid gap-2"
+                x-on:drop.prevent="$wire.uploadMultiple(
                 'files',
                 $event.dataTransfer.files,
                 () => {droping = false;progress = 0},
                 () => {droping = false;progress = 0},
                 e => {progress = e.detail.progress}
             )"
-            x-on:dragover.prevent="droping = true"
-            x-on:dragleave.prevent="droping = false">
+                x-on:dragover.prevent="droping = true"
+                x-on:dragleave.prevent="droping = false"
+                x-on:livewire-upload-finish="droping = false;progress = 0"
+                x-on:livewire-upload-error="droping = false;progress = 0"
+                x-on:livewire-upload-progress="progress = $event.detail.progress">
 
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <div class="text-center" x-show="droping && !progress">
-                    Drop Here
-                </div>
-                <div x-show="progress !== 0">
-                    <div class="text-center mb-3">
-                        Uploading...
-                    </div>
-                    <div class="progress" style="width: 70vw">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-main-500" x-bind:style="{ width: `${progress}%` }">
+                <label class="btn btn-outline-main-500 px-3 py-2 d-hidden"
+                    x-show="!progress"
+                    x-bind:style="{ 'border-style': droping ? 'dashed' : 'solid' }">
+
+                    <input type="file" wire:model="files" class="d-none" accept="image/*" multiple>
+                    Upload File
+                </label>
+                <div x-show="progress">
+                    <div class="progress border border-1 border-main-500 text-main-500 bg-transparent fs-6" style="height:unset">
+                        <div class="progress-bar px-3 py-2 progress-bar-striped progress-bar-animated bg-main-500"
+                            x-bind:style="{ width: `${progress}%` }">
+                            &nbsp;
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-12" x-show="!droping">
         <div class="row">
             @if ($photos->count())
                 @foreach ($photos as $photo)
