600728fa1cd83b8b2ed91fc2d1c632ff825473e8
yuma nishimura <sulfur.monoxide168@mgmail.com>
2022-08-12 17:17:14 +0900

    ファイルアップロード 認証制限

--- resources/views/livewire/photo/index.blade.php
+++ resources/views/livewire/photo/index.blade.php
@@ -1,40 +1,42 @@
 <div class="row">
     <div class="col-12">
-        <div class="row mb-3"
-            x-data="{ droping: false, progress: 0 }"
-            x-cloak>
+        @auth
+            <div class="row mb-3"
+                x-data="{ droping: false, progress: 0 }"
+                x-cloak>
 
-            <div class="col-md-10 offset-md-1 col-12 d-grid gap-2"
-                x-on:drop.prevent="$wire.uploadMultiple(
-                'files',
-                $event.dataTransfer.files,
-                () => {droping = false;progress = 0},
-                () => {droping = false;progress = 0},
-                e => {progress = e.detail.progress}
-            )"
-                x-on:dragover.prevent="droping = true"
-                x-on:dragleave.prevent="droping = false"
-                x-on:livewire-upload-finish="droping = false;progress = 0"
-                x-on:livewire-upload-error="droping = false;progress = 0"
-                x-on:livewire-upload-progress="progress = $event.detail.progress">
+                <div class="col-md-10 offset-md-1 col-12 d-grid gap-2"
+                    x-on:drop.prevent="$wire.uploadMultiple(
+                    'files',
+                    $event.dataTransfer.files,
+                    () => {droping = false;progress = 0},
+                    () => {droping = false;progress = 0},
+                    e => {progress = e.detail.progress}
+                )"
+                    x-on:dragover.prevent="droping = true"
+                    x-on:dragleave.prevent="droping = false"
+                    x-on:livewire-upload-finish="droping = false;progress = 0"
+                    x-on:livewire-upload-error="droping = false;progress = 0"
+                    x-on:livewire-upload-progress="progress = $event.detail.progress">
 
-                <label class="btn btn-outline-main-500 px-3 py-2 d-hidden"
-                    x-show="!progress"
-                    x-bind:style="{ 'border-style': droping ? 'dashed' : 'solid' }">
+                    <label class="btn btn-outline-main-500 px-3 py-2 d-hidden"
+                        x-show="!progress"
+                        x-bind:style="{ 'border-style': droping ? 'dashed' : 'solid' }">
 
-                    <input type="file" wire:model="files" class="d-none" accept="image/*" multiple>
-                    Upload File
-                </label>
-                <div x-show="progress">
-                    <div class="progress border border-1 border-main-500 text-main-500 bg-transparent fs-6" style="height:unset">
-                        <div class="progress-bar px-3 py-2 progress-bar-striped progress-bar-animated bg-main-500"
-                            x-bind:style="{ width: `${progress}%` }">
-                            &nbsp;
+                        <input type="file" wire:model="files" class="d-none" accept="image/*" multiple>
+                        Upload File
+                    </label>
+                    <div x-show="progress">
+                        <div class="progress border border-1 border-main-500 text-main-500 bg-transparent fs-6" style="height:unset">
+                            <div class="progress-bar px-3 py-2 progress-bar-striped progress-bar-animated bg-main-500"
+                                x-bind:style="{ width: `${progress}%` }">
+                                &nbsp;
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endauth
         <div class="row">
             @if ($photos->count())
                 @foreach ($photos as $photo)
