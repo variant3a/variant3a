1b6716e3682e086a467e0dc59ee82d52b5cdfc13
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-11-03 16:47:51 +0900

    footerの影響で一部全体レイアウトが崩れる問題を修正

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="relative w-screen my-8 flex flex-col justify-center items-center text-neutral-400 text-center pointer-events-none">
+<div class="relative my-8 flex flex-col justify-center items-center text-neutral-400 text-center pointer-events-none">
     <div class="grid grid-cols-1 sm:grid-cols-1 w-fit">
         <a href="{{ route('privacy-policy') }}" class="block mx-5 my-1 text-teal-500 cursor-pointer hover:underline pointer-events-auto">
             Privacy
