99053ffeb07bde6542357aa1b68a078cb0e3f611
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-11-03 13:38:47 +0900

    投稿リンクコピー機能実装

--- app/Http/Livewire/Post/Show.php
+++ app/Http/Livewire/Post/Show.php
@@ -9,6 +9,7 @@ class Show extends Component
 {
 
     public string $title = '';
+    public string $share_string = '';
     public $post;
 
     public function mount($id)
@@ -16,6 +17,7 @@ class Show extends Component
         $post = Post::find($id);
         $this->post = $post;
         $this->title = $post->title;
+        $this->share_string = $post->title . ' - ' . env('APP_NAME', 'Laravel') . "\n" . url()->current();
     }
 
     public function render()
--- resources/views/livewire/post/show.blade.php
+++ resources/views/livewire/post/show.blade.php
@@ -1,12 +1,13 @@
 <div class="grid grid-cols-1 gap-1 md:grid-cols-4 sm:gap-3">
     <div class="col-span-4 md:col-span-1">
-        <div class="relative md:sticky md:top-24">
+        <div class="relative md:sticky md:top-24"
+            x-data="{ showShareDropdown: false }">
             @auth
                 <a href="{{ route('post.edit', ['id' => $post->id]) }}" class="block w-full p-2 mb-2 font-bold text-center bg-teal-500 rounded shadow sm:mb-3 hover:shadow-lg hover:bg-teal-400/90 text-neutral-200 ring-1 ring-black/5">
                     Edit
                 </a>
             @endauth
-            <div class="flex p-2 bg-white rounded shadow md:flex-col sm:p-3 h-fit dark:bg-zinc-700 text-neutral-700 dark:text-neutral-200 ring-1 ring-black/5">
+            <div class="flex mb-1 sm:mb-3 p-2 bg-white rounded shadow md:flex-col sm:p-3 h-fit dark:bg-zinc-700 text-neutral-700 dark:text-neutral-200 ring-1 ring-black/5">
                 <div class="flex flex-col justify-center flex-shrink-0 md:mb-3 md:flex-row">
                     <div class="w-16 md:w-1/2 h-full md:h-0 pt-16 md:pt-[50%] relative aspect-square">
                         <img src="{{ asset('storage/' . $post->user->profile_photo_path) }}" class="absolute inset-0 object-cover w-full h-full rounded-xl">
@@ -33,6 +34,22 @@
                             {!! nl2br(e($post->user->bio)) !!}
                         </p>
                     @endif
+                </div>
+            </div>
+            <button type="button" class="w-full p-2 flex justify-between font-bold text-center bg-teal-500 rounded shadow hover:shadow-lg hover:bg-teal-400/90 text-neutral-200 ring-1 ring-black/5"
+                x-on:click="showShareDropdown = !showShareDropdown">
+                <i class="bi bi-chevron-down invisible"></i>
+                Share
+                <i class="bi bi-chevron-down"></i>
+            </button>
+            <div class="absolute mt-1 sm:mt-3 z-30 w-full right-0 rounded shadow-lg backdrop-blur-lg bg-zinc-200/20 dark:bg-white/10 ring-1 ring-white/40 dark:ring-white/20 focus:outline-none" x-show="showShareDropdown" x-cloak x-transition>
+                <div class="py-1">
+                    <button type="button" class="block w-full px-4 py-2 text-sm text-left text-neutral-700 dark:text-neutral-200 hover:text-neutral-200 hover:bg-teal-500/50"
+                        x-on:click="
+                            navigator.clipboard.writeText($wire.share_string);
+                            showShareDropdown = !showShareDropdown;">
+                        Copy link to clipboard
+                    </button>
                 </div>
             </div>
         </div>
