de600a227360c3ca0ce379eef9a5bd130b5a85c7
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-11-03 16:05:08 +0900

    footerでpointer操作に支障が出る問題を修正

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<div class="relative w-screen my-8 flex flex-col justify-center items-center text-neutral-400 text-center">
+<div class="relative w-screen my-8 flex flex-col justify-center items-center text-neutral-400 text-center pointer-events-none">
     <div class="grid grid-cols-1 sm:grid-cols-1 w-fit">
-        <a href="{{ route('privacy-policy') }}" class="block mx-5 my-1 text-teal-500 cursor-pointer hover:underline">
+        <a href="{{ route('privacy-policy') }}" class="block mx-5 my-1 text-teal-500 cursor-pointer hover:underline pointer-events-auto">
             Privacy
         </a>
     </div>
