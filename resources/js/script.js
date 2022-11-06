1f153a7b7f27dac19c62c7da50a194f78c0f5beb
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-11-07 08:08:48 +0900

    hljsがpost編集中正常に発火するよう修正

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,27 +3,14 @@ import * as Turbo from "@hotwired/turbo"
 
 Turbo.start()
 
-document.addEventListener('turbo:load', function () {
-    OnLoad()
-})
-
-window.addEventListener('paginated', () => {
-    OnLoad()
-})
-
-window.addEventListener('post-updated', () => {
-    hljs.highlightAll()
-})
-
-function OnLoad() {
-    hljs.highlightAll()
-}
+document.addEventListener('turbo:load', () => hljs.highlightAll())
+Livewire.hook('element.updated', () => hljs.highlightAll())
 
 window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js').
             then(() => {
-                console.log('ServiceWorker registered')
+                //
             }).
             catch((error) => {
                 console.warn('ServiceWorker error', error)
