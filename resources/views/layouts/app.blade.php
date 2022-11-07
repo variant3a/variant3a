7f3663d54adb604ecbbe28fe6f8261343f783acc
yuma nishimura <sulfur.monoxide168@gmail.com>
2022-11-07 11:30:08 +0900

    canonical tag設置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Canonical Tag -->
+    <link rel="canonical" href="{{ url()->current() }}">
+
     <!-- PWA -->
     <link rel="manifest" href="{{ asset('manifest.json') }}">
 
