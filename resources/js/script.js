a8d5c630bf24a94283ff05a4134569c94fb869a3
yuma nishimura <sulfur.monoxide168@gmail.com>
2023-01-29 23:20:57 +0900

    hljs docker apache追加

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,12 @@
 import hljs from 'highlight.js/es/common'
+import apache from 'highlight.js/es/languages/apache'
+import dockerfile from 'highlight.js/es/languages/dockerfile'
 import nginx from 'highlight.js/es/languages/nginx'
 import powershell from 'highlight.js/es/languages/powershell'
 import * as Turbo from "@hotwired/turbo"
 
+hljs.registerLanguage('apache', apache)
+hljs.registerLanguage('dockerfile', dockerfile)
 hljs.registerLanguage('nginx', nginx)
 hljs.registerLanguage('powershell', powershell)
 
