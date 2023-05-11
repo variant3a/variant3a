0e5c6cf4e7e23cbd81b804e06f51a1864baabe3a
yuma nishimura <sulfur.monoxide168@gmail.com>
2023-05-11 15:35:55 +0900

    Google タグ設置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,19 @@
     <meta name="google-site-verification" content="2ZseLlPEz2jQX_FTIz1UkNzLEpovoPTGEdn0S4YgbzE" />
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-41VLYNFZ5Q"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-41VLYNFZ5Q');
+</script>
+
 <body class="flex flex-col min-h-screen bg-gray-100 dark:bg-zinc-800">
     <header id="header" data-turbo-permanent>
         @include('components.header')
